dd37c97c5e4e9c4450ca4211c707d27a8210e428
Cédric Millauriaux <cmillauriaux@icysoft.fr>
2017-10-20 16:45:54 +0200

    Modification des messages

--- console.js
+++ console.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const mustache = require("mustache-express")
-const xml = require("xml")
+const express = require("express");
+const mustache = require("mustache-express");
+const xml = require("xml");
 
-const app = express() 
+const app = express();
 
 app.use(express.static("static"));
 app.use(express.static("node_modules/bootstrap/dist"));
@@ -14,13 +14,13 @@ app.set("view engine", "mustache");
 
 app.get("/", function (req, res) {
   const messages = [
-    { titre: "Message A", contenu: "Ceci est le message A" },
-    { titre: "Message B", contenu: "Ceci est le message B" },
-    { titre: "Message C", contenu: "Ceci est le message C" }
+    { titre: "Message A", contenu: "Ceci est le message A !" },
+    { titre: "Message B", contenu: "Ceci est le message B !" },
+    { titre: "Message C", contenu: "Ceci est le message C !" }
   ];
 
   returnToClient(messages, "user.mustache", req, res);
-}) 
+});
 
 function returnToClient (resultat, nomTemplate, requete, response) {
   switch (requete.accepts(["html", "json", "xml"])) {
@@ -46,16 +46,16 @@ app.get("/user/:name*?", function (req, res) {
   ];
 
   returnToClient(messages, "user.mustache", req, res);
-})
+});
 
 app.post("/prod/add", (req, res) => {
   console.log(req.body);
   res.sendStatus(200);
-})
+});
 
 app.listen(8080, function () {
   console.log("Serveur HTTP lancé !");
-})
+});
 
 function returnXML (object, response) {
   response.set("Content-Type", "text/xml");
